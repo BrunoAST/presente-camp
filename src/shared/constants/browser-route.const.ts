c7284aa7e6bf4366dcb66929c12ccc2ee62de64e
Bruno Alves <bruno-alves2010@live.com>
2021-05-19 22:49:48 -0300

    Routes

--- src/shared/constants/browser-route.const.ts
+++ src/shared/constants/browser-route.const.ts
@@ -1,6 +1,6 @@
 export const BrowserRoutes = {
-    DEFAULT: '//*',
-    LANDING: '/home',
+    DEFAULT: '/',
+    LANDING: '/',
     SIGN_IN: '/acessar',
     SIGN_UP_TYPE: '/tipo-de-cadastro',
     SIGN_UP_STUDENT: '/cadastro-aluno',
