eb77103b83140d6d45a4f1aa36d72343c30cc41a
Bruno Alves <bruno-alves2010@live.com>
2021-05-22 17:32:17 -0300

    Navbar

--- src/pages/Default/Default.tsx
+++ src/pages/Default/Default.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Outlet} from 'react-router-dom';
-import Navbar from '../../shared/components/Navbar/Navbar';
+import Navbar from 'shared/components/Navbar/Navbar';
 
 const Default = () => {
     return (
--- src/shared/components/Navbar/Navbar.tsx
+++ src/shared/components/Navbar/Navbar.tsx
@@ -2,40 +2,34 @@ import React, {memo} from 'react';
 import {Link} from 'react-router-dom';
 
 import style from './navbar.module.css';
-import useIsMobile from '../../hooks/useIsMobile';
 import {BrowserRoutes} from '../../constants/browser-route.const';
 import LogoMobileDesktop from '../LogoMobileDesktop/LogoMobileDesktop';
 import {ELogoMobileDesktop} from '../LogoMobileDesktop/interface/logo-mobile-desktop.interface';
 
 const Navbar = () => {
-    const {isMobile} = useIsMobile();
-
     return (
         <header className={`${style.header}`}>
             <nav className={`${style.nav}`}>
-                <LogoMobileDesktop type={ELogoMobileDesktop.NORMAL} />
+                <LogoMobileDesktop type={ELogoMobileDesktop.NORMAL}/>
 
-                {!isMobile && <ul>
+                <ul>
                     <li className={style.links}>
-                        <Link data-cy="nab-sign-in-link" className={style.linksText} to={BrowserRoutes.SIGN_IN}>
+                        <Link
+                            data-cy="nab-sign-in-link"
+                            className={`${style.linksText} ${style.signIn}`}
+                            to={BrowserRoutes.SIGN_IN}
+                        >
                             Acessar
                         </Link>
-                        <Link data-cy="nab-sign-up-link" className={style.linksText} to={BrowserRoutes.SIGN_UP}>
+                        <Link
+                            data-cy="nab-sign-up-link"
+                            className={`${style.linksText} ${style.signUp}`}
+                            to={BrowserRoutes.SIGN_UP}
+                        >
                             Cadastre-se
                         </Link>
                     </li>
-                </ul>}
-
-                {isMobile && <ul className={style.listMobile}>
-                    <li className={style.linksMobile}>
-                        <Link data-cy="nab-sign-in-link" className={style.linksText} to={BrowserRoutes.SIGN_IN}>
-                            Acessar
-                        </Link>
-                        <Link data-cy="nab-sign-up-link" className={style.linksText} to={BrowserRoutes.SIGN_UP}>
-                            Cadastre-se
-                        </Link>
-                    </li>
-                </ul>}
+                </ul>
             </nav>
         </header>
     );
