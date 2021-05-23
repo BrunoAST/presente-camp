790dec419a86e46650350e1cdb423d6be2124944
Bruno Alves <bruno-alves2010@live.com>
2021-05-23 13:31:59 -0300

    Content section

--- src/shared/components/RegisterContainer/RegisterContainer.tsx
+++ src/shared/components/RegisterContainer/RegisterContainer.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import style from './register-container.module.css';
 import bg from 'assets/Background/BackgoundBlur.png';
-import LogoMobileDesktop from '../LogoMobileDesktop/LogoMobileDesktop';
-import {ELogoMobileDesktop} from '../LogoMobileDesktop/interface/logo-mobile-desktop.interface';
 
 const RegisterContainer: React.FC = ({children}) => {
     return (
@@ -11,10 +9,6 @@ const RegisterContainer: React.FC = ({children}) => {
             <img className="background" src={bg} alt="Background"/>
 
             <div className={style.overlay}>
-                <picture className={style.logo}>
-                    <LogoMobileDesktop type={ELogoMobileDesktop.CONTRAST}/>
-                </picture>
-
                 <article className={style.content}>
                     {children}
                 </article>
